134fddf6a7d5385a7f785ea9266da0131501d315
Natalia Khrol <natalia_khrol@epam.com>
2026-01-07 00:24:30 +0300

    Parametrization is added

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,7 @@
 import time
 
+import pytest
+
 from data.user_factory import UserFactory
 from pages.login_page import LoginPage
 from pages.products_page import ProductsPage
@@ -19,8 +21,20 @@ class TestLoginPage:
         assert products_page.page_is_open() == True
         assert products_page.check_title() == "Products"
 
-    def test_locked_user_login(self, driver):
-        user = UserFactory.locked()
+    @pytest.mark.parametrize(
+        "user, error_message",
+        [
+            (UserFactory.locked(), "Epic sadface: Sorry, this user has been locked out."),
+            (UserFactory.empty_username(), "Epic sadface: Username is required"),
+            (UserFactory.empty_password(), "Epic sadface: Password is required"),
+            (UserFactory.wrong_username(),
+                    "Epic sadface: Username and password do not match any user in this service"
+            )
+        ]
+    )
+
+    def test_incorrect_login(self, driver, user, error_message):
+       # user = UserFactory.locked()
         login_page = LoginPage(driver, user)
         login_page.open("https://www.saucedemo.com/")
         time.sleep(3)
@@ -29,44 +43,43 @@ class TestLoginPage:
 
         assert login_page.is_error_message_appear(), "Сообщение об ошибке не появилось для заблокированного юзера"
 
-        assert login_page.get_error_message() == "Epic sadface: Sorry, this user has been locked out.", \
-            "Сообщение об ошибке не верное"
+        assert login_page.get_error_message() == error_message, "Сообщение об ошибке не верное"
 
-    def test_empty_user_login(self, driver):
-        user = UserFactory.empty_username()
-        login_page = LoginPage(driver, user)
-        login_page.open("https://www.saucedemo.com/")
-        time.sleep(3)
-        login_page.fill_in_login_form()
-        time.sleep(5)
-
-        assert login_page.is_error_message_appear(), "Сообщение об ошибке не появилось для заблокированного юзера"
-
-        assert login_page.get_error_message() == "Epic sadface: Username is required", \
-            "Сообщение об ошибке не верное"
-
-    def test_empty_password_login(self, driver):
-        user = UserFactory.empty_password()
-        login_page = LoginPage(driver, user)
-        login_page.open("https://www.saucedemo.com/")
-        time.sleep(3)
-        login_page.fill_in_login_form()
-        time.sleep(5)
-
-        assert login_page.is_error_message_appear(), "Сообщение об ошибке не появилось для заблокированного юзера"
-
-        assert login_page.get_error_message() == "Epic sadface: Password is required", \
-                "Сообщение об ошибке не верное"
-
-    def test_non_match_login(self, driver):
-        user = UserFactory.wrong_username()
-        login_page = LoginPage(driver, user)
-        login_page.open("https://www.saucedemo.com/")
-        time.sleep(3)
-        login_page.fill_in_login_form()
-        time.sleep(5)
-
-        assert login_page.is_error_message_appear(), "Сообщение об ошибке не появилось для заблокированного юзера"
-
-        assert login_page.get_error_message() == "Epic sadface: Username and password do not match any user in this service", \
-                "Сообщение об ошибке не верное"
+    # def test_empty_user_login(self, driver):
+    #     user = UserFactory.empty_username()
+    #     login_page = LoginPage(driver, user)
+    #     login_page.open("https://www.saucedemo.com/")
+    #     time.sleep(3)
+    #     login_page.fill_in_login_form()
+    #     time.sleep(5)
+    #
+    #     assert login_page.is_error_message_appear(), "Сообщение об ошибке не появилось для заблокированного юзера"
+    #
+    #     assert login_page.get_error_message() == "Epic sadface: Username is required", \
+    #         "Сообщение об ошибке не верное"
+    #
+    # def test_empty_password_login(self, driver):
+    #     user = UserFactory.empty_password()
+    #     login_page = LoginPage(driver, user)
+    #     login_page.open("https://www.saucedemo.com/")
+    #     time.sleep(3)
+    #     login_page.fill_in_login_form()
+    #     time.sleep(5)
+    #
+    #     assert login_page.is_error_message_appear(), "Сообщение об ошибке не появилось для заблокированного юзера"
+    #
+    #     assert login_page.get_error_message() == "Epic sadface: Password is required", \
+    #             "Сообщение об ошибке не верное"
+    #
+    # def test_non_match_login(self, driver):
+    #     user = UserFactory.wrong_username()
+    #     login_page = LoginPage(driver, user)
+    #     login_page.open("https://www.saucedemo.com/")
+    #     time.sleep(3)
+    #     login_page.fill_in_login_form()
+    #     time.sleep(5)
+    #
+    #     assert login_page.is_error_message_appear(), "Сообщение об ошибке не появилось для заблокированного юзера"
+    #
+    #     assert login_page.get_error_message() == "Epic sadface: Username and password do not match any user in this service", \
+    #             "Сообщение об ошибке не верное"
