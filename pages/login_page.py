230248eeaa5db2ff9d899a2955ec6972b4883de0
Natalia Khrol <natalia_khrol@epam.com>
2026-01-10 16:17:22 +0300

    Decorator @allure.step are added

--- pages/login_page.py
+++ pages/login_page.py
@@ -16,18 +16,23 @@ class LoginPage(BasePage):
         self.element_is_visible(self.locators.USER_INPUT).send_keys(self.user.username)
         self.element_is_visible(self.locators.USER_INPUT).send_keys(Keys.CONTROL + "a")
         self.element_is_visible(self.locators.USER_INPUT).send_keys(Keys.BACK_SPACE)
-        #self.element_is_visible(self.locators.USER_INPUT).clear()
+        # self.element_is_visible(self.locators.USER_INPUT).clear()
         time.sleep(3)
         self.element_is_visible(self.locators.USER_INPUT).send_keys(self.user.username)
         self.element_is_visible(self.locators.PASSWORD_INPUT).send_keys(self.user.password)
         self.element_is_visible(self.locators.SUBMIT_BUTTON).click()
         time.sleep(5)
 
+    @allure.step("Проверка появления сообщения об ошибке")
     def is_error_message_appear(self) -> bool:
         try:
             return bool(self.element_is_visible(self.locators.ERROR_MESSAGE, timeout=3))
         except TimeoutException:
             return False
 
+    @allure.step("Получение текста сообщения об ошибке")
     def get_error_message(self) -> str:
-        return self.element_is_visible(self.locators.ERROR_MESSAGE).text
+        text = self.element_is_visible(self.locators.ERROR_MESSAGE).text
+        #!!!!!!!!!!!!!!!!!!!!!!!!!!!! доп шаг ниже для отчета Прикрепляем сам текст ошибки в отчет для удобства анализа
+        allure.attach(text, name="Текст ошибки", attachment_type=allure.attachment_type.TEXT)
+        return text
--- pages/products_page.py
+++ pages/products_page.py
@@ -1,3 +1,5 @@
+import allure
+
 from locators.products_page_locators import ProductsPageLocators
 from pages.base_page import BasePage
 
@@ -5,23 +7,29 @@ from pages.base_page import BasePage
 class ProductsPage(BasePage):
     locators = ProductsPageLocators()
 
+    @allure.step("Проверка: страница открыта")
     def page_is_open(self):
         return self.element_is_visible(self.locators.PAGE_TITLE).is_displayed()
 
+    @allure.step("Получение заголовка страницы")
     def check_title(self):
         return self.element_is_visible(self.locators.PAGE_TITLE).text
 
+    @allure.step("Добавление товара '{product}' в корзину по названию")
     def add_goods_to_cart_by_name(self, product):
         by, pattern = self.locators.ADD_TO_CART_BUTTON_PATTERN
         locator = (by, pattern.format(product))
         self.element_is_visible(locator).click()
 
+    @allure.step("Проверка количества товаров в корзине (на иконке)")
     def check_goods_quantity(self) -> str:
         return self.element_is_visible(self.locators.CART_BADGE).text
 
+    @allure.step("Добавление товара с индексом {product_index} в корзину")
     def add_goods_to_cart_by_index(self, product_index):
         buttons = self.elements_are_visible(self.locators.ADD_TO_CART_BUTTON)
         buttons[product_index].click()
 
+    @allure.step("Переход в корзину (клик по иконке)")
     def switch_to_cart(self):
         self.element_is_visible(self.locators.CART_LINK).click()
